717befc36034d832b5bcbb7465b779b262a88899
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-02-12 11:26:34 -0500

    redo complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace LINQ
+﻿using System.Diagnostics;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
+
+            var videoGames = new List<string>() 
+            { 
+                "Minecraft",
+                "Super Mario Bros", 
+                "Halo", 
+                "The Last Of Us",
+                "Zombies",
+                "Resident Evil",
+                "Grand Theft Auto",
+                "Roblox",
+                "Fortnite",
+                "The Legend of Zelda"
+            };
+
+            var sortedGames = videoGames.OrderBy(x => x.Length).ToList();
+
+            Console.WriteLine("Video games sorted by name length:");
+
+            foreach(var game in sortedGames)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
